68742215518c0d5692c787bdd9e102ec7455f3c0
Maysam Torabi <kherad@gmail.com>
2019-08-12 11:05:34 +0430

    styling and error handling

--- events.js
+++ events.js
@@ -8,124 +8,151 @@ function newGuess(wordIndex) {
 }
 
 function stopGame() {
- if (redWords.length == 0 && timing) {
-   timing = false
-   //  unset the clockHandle
-   word = null
-   if ($('#submit_details').is(':checked')) {
-     var score = calculateScore()
-     var letters = countLetters()
-     if(navigator.onLine)
-       $.get('scores.php?time='+time+'&score='+score+'&fullname='+$('#fullname').val()+'&email='+$('#email').val()+'&wordcount='+ wordCount + '&letters=' + letters , function(data) { if(data) alert(data) } )
-     //  send data, store recoords
-   }
- }
+  if (redWords.length == 0 && timing) {
+    timing = false
+    //  unset the clockHandle
+    word = null
+    if ($('#submit_details').is(':checked')) {
+      var score = calculateScore()
+      var letters = countLetters()
+      if (navigator.onLine)
+        $.get(
+          'scores.php?time=' +
+            time +
+            '&score=' +
+            score +
+            '&fullname=' +
+            $('#fullname').val() +
+            '&email=' +
+            $('#email').val() +
+            '&wordcount=' +
+            wordCount +
+            '&letters=' +
+            letters,
+          function(data) {
+            if (data) alert(data)
+          }
+        )
+      //  send data, store recoords
+    }
+  }
 }
 
-function mouseClick(e){
- e=e.originalEvent
- if ( $('#setting_panel').css('display') == 'block' ) {
-   return true
- }
+function mouseClick(e) {
+  e = e.originalEvent
+  if ($('#setting_panel').css('display') == 'block') {
+    return true
+  }
 
- var x = (e.touches) ? e.touches[0].pageX : e.pageX;
- var y = (e.touches) ? e.touches[0].pageY : e.pageY;
+  var x = e.touches ? e.touches[0].pageX : e.pageX
+  var y = e.touches ? e.touches[0].pageY : e.pageY
 
- x = x - $("#canvas").offset().left
- y = y - $("#canvas").offset().top
- _.each(events, function(ev){
-   if (ev[0] <= x && x <= ev[0] + ev[2] && ev[1] <= y && y <= ev[1] + ev[3]) {
-        ev[4](x, y);
-        drawPuzzle()
+  x = x - $('#canvas').offset().left
+  y = y - $('#canvas').offset().top
+  _.each(events, function(ev) {
+    if (ev[0] <= x && x <= ev[0] + ev[2] && ev[1] <= y && y <= ev[1] + ev[3]) {
+      ev[4](x, y)
+      drawPuzzle()
     }
-   })
+  })
 }
 function detectWord(i, j, current_word) {
- //  returns the index of the word which lays on (i,j)
- for (var k = 0; k < wordCount; k++) {
-   tmp_word = wordList[k]
-   if(tmp_word.startx <= i && i<= tmp_word.endx && tmp_word.starty <= j && j<= tmp_word.endy && tmp_word != current_word) {
-     return tmp_word
-   }
- }
- return current_word
+  //  returns the index of the word which lays on (i,j)
+  for (var k = 0; k < wordCount; k++) {
+    tmp_word = wordList[k]
+    if (
+      tmp_word.startx <= i &&
+      i <= tmp_word.endx &&
+      tmp_word.starty <= j &&
+      j <= tmp_word.endy &&
+      tmp_word != current_word
+    ) {
+      return tmp_word
+    }
+  }
+  return current_word
 }
 
-
-function mouseDoubleClick(e){
+function mouseDoubleClick(e) {
   e.preventDefault()
   e.stopPropagation()
-   if(e.originalEvent)
-     e=e.originalEvent
-   if ( !puzzle || $('#setting_panel').css('display') == 'block' ) {
-     return true;
-   }
+  if (e.originalEvent) e = e.originalEvent
+  if (!puzzle || $('#setting_panel').css('display') == 'block') {
+    return true
+  }
 
- var x = (e.touches) ? e.touches[0].pageX : e.pageX;
- var y = (e.touches) ? e.touches[0].pageY : e.pageY;
+  var x = e.touches ? e.touches[0].pageX : e.pageX
+  var y = e.touches ? e.touches[0].pageY : e.pageY
 
- x = Math.floor((x-$("#canvas").offset().left -PUZZLE_LEFT) / SIZE);
- y = Math.floor((y-$("#canvas").offset().top -PUZZLE_TOP) / SIZE);
+  x = Math.floor((x - $('#canvas').offset().left - PUZZLE_LEFT) / SIZE)
+  y = Math.floor((y - $('#canvas').offset().top - PUZZLE_TOP) / SIZE)
 
- if (0<=x && x< COLUMNS && 0<=y && y< ROWS) {
-   if ( puzzle[x][y] != null && puzzle[x][y] != -1 && hints[x][y] == null) {
-     hint = hints[x][y] = puzzle[x][y];
-     //  remove all guesses without this hint
-     var _word = null
-     for (var i_temp = 0; i_temp < 2; i_temp++) //  TODO: refactor this
-      if(_word = detectWord(x,y,_word))
-      {
-        i = _word.index
-       //  check if the hint is for this word
-       k = x + y - _word.startx - _word.starty;
-       _word.hint[k] = hint
-       for (var j = guesses[i].length - 2; j >= 0; j--) {
-         _guess = guesses[i][j]
-         if (_guess[k] != hint) {
-           //  remove guess
-           removeGuess(i, j)
-         }
-       }
-       var _guess = guesses[i][guesses[i].length -1]
-       _guess[k] = hint
-       //  if everything is not hinted, and guess is complete, check for word
-       if (_word.hint.join('').length == _word.length + 1) {
-         // remove guess if repeated
-         // no need to remove, we don't show the last guess for full hints if repeated
-         repeatFlag = guesses[i].some(function (x) { return (x.join() == _guess.join() && x !== _guess) })
-         if (repeatFlag) {
-           guesses[i].pop()
-           if (word === _word) {
-             current_guess = guesses[i][guesses[i].length -1]
-           }
-           addGuess(i)
-           guesses[i].push(newGuess(i))
-           current_guess = guesses[i][guesses[i].length -1]
-         }
-         continue
-       }
-       if (_guess.join('').length == _word.length + 1 ) {
-         //  check the guess
+  if (0 <= x && x < COLUMNS && 0 <= y && y < ROWS) {
+    if (puzzle[x][y] != null && puzzle[x][y] != -1 && hints[x][y] == null) {
+      hint = hints[x][y] = puzzle[x][y]
+      //  remove all guesses without this hint
+      var _word = null
+      for (
+        var i_temp = 0;
+        i_temp < 2;
+        i_temp++ //  TODO: refactor this
+      )
+        if ((_word = detectWord(x, y, _word))) {
+          i = _word.index
+          //  check if the hint is for this word
+          k = x + y - _word.startx - _word.starty
+          _word.hint[k] = hint
+          for (var j = guesses[i].length - 2; j >= 0; j--) {
+            _guess = guesses[i][j]
+            if (_guess[k] != hint) {
+              //  remove guess
+              removeGuess(i, j)
+            }
+          }
+          var _guess = guesses[i][guesses[i].length - 1]
+          _guess[k] = hint
+          //  if everything is not hinted, and guess is complete, check for word
+          if (_word.hint.join('').length == _word.length + 1) {
+            // remove guess if repeated
+            // no need to remove, we don't show the last guess for full hints if repeated
+            repeatFlag = guesses[i].some(function(x) {
+              return x.join() == _guess.join() && x !== _guess
+            })
+            if (repeatFlag) {
+              guesses[i].pop()
+              if (word === _word) {
+                current_guess = guesses[i][guesses[i].length - 1]
+              }
+              addGuess(i)
+              guesses[i].push(newGuess(i))
+              current_guess = guesses[i][guesses[i].length - 1]
+            }
+            continue
+          }
+          if (_guess.join('').length == _word.length + 1) {
+            //  check the guess
 
-         if(_guess.join('').length > word.length) {
-           found = _.indexOf(checkList, _guess.join(''));
-           if ( found != -1) {
-             //  is it a new guess?
-             repeatFlag = guesses[i].some(function (x) { return (x.join() == _guess.join() && x !== _guess) })
-             if(!repeatFlag) {
-               //  add to the list
-               addGuess(i)
-               var g = guesses[i].push(newGuess(i));
-               if (word === _word) {
-                 current_guess = guesses[i][g-1];
-               }
-             }
-           }
-         }
-       }
-     }
-   }
- }
- drawPuzzle()
- return false;
+            if (_guess.join('').length > word.length) {
+              found = _.indexOf(checkList, _guess.join(''))
+              if (found != -1) {
+                //  is it a new guess?
+                repeatFlag = guesses[i].some(function(x) {
+                  return x.join() == _guess.join() && x !== _guess
+                })
+                if (!repeatFlag) {
+                  //  add to the list
+                  addGuess(i)
+                  var g = guesses[i].push(newGuess(i))
+                  if (word === _word) {
+                    current_guess = guesses[i][g - 1]
+                  }
+                }
+              }
+            }
+          }
+        }
+    }
+  }
+  drawPuzzle()
+  return false
 }
--- index.js
+++ index.js
@@ -1,167 +1,177 @@
-document.addEventListener('touchmove', function(event) {
-  event.preventDefault();
-}, false);
+document.addEventListener(
+  'touchmove',
+  function(event) {
+    event.preventDefault()
+  },
+  false
+)
 
-$(window).bind('touchstart', function(e){
-       var now = new Date().getTime();
-       var lastTouch = $(this).data('lastTouch') || now + 1 /** the first time this will make delta a negative number */;
-       var delta = now - lastTouch;
-       if(delta<200 && delta>0){
-               // the second touchend event happened within half a second. Here is where we invoke the double tap code
-               mouseDoubleClick(e)
-       } else {
-               // A click/touch action could be invoked here but wee need to wait half a second before doing so.
-               mouseClick(e)
-       }
-       $(this).data('lastTouch', now);
+$(window).bind('touchstart', function(e) {
+  var now = new Date().getTime()
+  var lastTouch =
+    $(this).data('lastTouch') ||
+    now + 1 /** the first time this will make delta a negative number */
+  var delta = now - lastTouch
+  if (delta < 200 && delta > 0) {
+    // the second touchend event happened within half a second. Here is where we invoke the double tap code
+    mouseDoubleClick(e)
+  } else {
+    // A click/touch action could be invoked here but wee need to wait half a second before doing so.
+    mouseClick(e)
+  }
+  $(this).data('lastTouch', now)
 })
 function makeTree() {
-  dictionary = _.filter(dictionary, function (a){ return a.length >= MIN_LENGTH && a.length <= MAX_LENGTH })
-  _.each(dictionary, function(_word){
-   temp = tree;
-   for (var j = 0; j < _word.length; j++) {
-     //  where should the character be listed at
-     index = _word[j]; //  .charCodeAt(0)-65;
-     //  is this number in the list yet?
-     last = temp.indexOf(index) + 1;
-     if ( last == 0 ) {
-       // if not, add the ascii code and push an empty array after it
-       last = temp.push(index);
-       temp.push([j+1,temp]);
-     }
-     //  now temp is holding the new array or old one for this word
-     temp = temp[last];
-
-   };
-   //  push something to indicated if you are up to here, you have a valid word
-   if(!_.contains(temp, 'YES')) {
-     //  make sure it's not a duplicated word
-     temp.push('YES');
-   }
-   //  if tree has yes then take it as a word
- })
+  dictionary = _.filter(dictionary, function(a) {
+    return a.length >= MIN_LENGTH && a.length <= MAX_LENGTH
+  })
+  _.each(dictionary, function(_word) {
+    temp = tree
+    for (var j = 0; j < _word.length; j++) {
+      //  where should the character be listed at
+      index = _word[j] //  .charCodeAt(0)-65;
+      //  is this number in the list yet?
+      last = temp.indexOf(index) + 1
+      if (last == 0) {
+        // if not, add the ascii code and push an empty array after it
+        last = temp.push(index)
+        temp.push([j + 1, temp])
+      }
+      //  now temp is holding the new array or old one for this word
+      temp = temp[last]
+    }
+    //  push something to indicated if you are up to here, you have a valid word
+    if (!_.contains(temp, 'YES')) {
+      //  make sure it's not a duplicated word
+      temp.push('YES')
+    }
+    //  if tree has yes then take it as a word
+  })
 }
 var high_score = ''
 var top_score = ''
 var dictionary = easyList
-var tree = [0,null]
+var tree = [0, null]
 
 function supports_html5_storage() {
   try {
-    return 'localStorage' in window && window['localStorage'] !== null;
+    return 'localStorage' in window && window['localStorage'] !== null
   } catch (e) {
-    return false;
+    return false
   }
 }
 
 function newGame() {
- tries = 0
- resetGuessList()
- do {
-   tries++
-   if (tries>10000)
-     break
-   init();
-   var board = puzzle.slice(0);
-   for(var i=0; i<COLUMNS; i++) {
-     board[i] = board[i].slice(0);
-   }
-   if(wordList.length>0) {
-     isConnected(board, wordList[0].startx, wordList[0].starty);
-   }
-   notConnected = false;
-   for(var i=0; i<COLUMNS; i++) {
-     for(var j=0; j<ROWS; j++) {
-       if (board[i][j] != null && board[i][j] != -1) {
-         notConnected = true;
-         break;
-       }
-     }
-   }
- } while (notConnected || wordCount != $("#number_of_words:checked").val());
- timing = true
- time = 0
- if(navigator.onLine)
- $.ajax({
-   url: 'topscores.php',
-   dataType: 'json',
-   async: true,
-   data: {"wordcount": wordCount, "fullname": $('#fullname').val()},
-   success: function(data) {
-     high_score = data.HS
-     top_score=data.TS
-     drawPuzzle()
-     $('#loading_panel').hide()
-   }
-  })
+  tries = 0
+  resetGuessList()
+  do {
+    tries++
+    if (tries > 10000) break
+    init()
+    var board = puzzle.slice(0)
+    for (var i = 0; i < COLUMNS; i++) {
+      board[i] = board[i].slice(0)
+    }
+    if (wordList.length > 0) {
+      isConnected(board, wordList[0].startx, wordList[0].starty)
+    }
+    notConnected = false
+    for (var i = 0; i < COLUMNS; i++) {
+      for (var j = 0; j < ROWS; j++) {
+        if (board[i][j] != null && board[i][j] != -1) {
+          notConnected = true
+          break
+        }
+      }
+    }
+  } while (notConnected || wordCount != $('#number_of_words:checked').val())
+  timing = true
+  time = 0
+  if (navigator.onLine)
+    $.ajax({
+      url: 'topscores.php',
+      dataType: 'json',
+      async: true,
+      data: { wordcount: wordCount, fullname: $('#fullname').val() },
+      success: function(data) {
+        high_score = data.HS
+        top_score = data.TS
+        drawPuzzle()
+        $('#loading_panel').hide()
+      },
+      error: function() {
+        console.log('There was an error fetching topscores!')
+        $('#loading_panel').hide()
+      }
+    })
   else {
-     drawPuzzle()
-     $('#loading_panel').hide()
+    drawPuzzle()
+    $('#loading_panel').hide()
   }
-
 }
 
-$(window).load(function () {
+$(window).load(function() {
   makeTree()
- $("#setting_panel").hide()
- if (supports_html5_storage()) {
-   if(localStorage.fullname)
-     $('#fullname').val(localStorage.fullname)
-   if(localStorage.email)
-     $('#email').val(localStorage.email)
- }
-
- window.ondblclick = mouseDoubleClick
-  function mouseMove(e) {
-   if(e.originalEvent)
-         e=e.originalEvent
-        e = e || window.event;
-        if (!e.which && e.button) {
-            if      (e.button & 1) e.which = 1;
-            else if (e.button & 4) e.which = 2;
-            else if (e.button & 2) e.which = 3;
-        }
-        if (e.buttons != undefined)
-         button = e.buttons
-        else
-         button = e.which
-        if (button) {
-         //  scroll if scrollbar is there
-
-     var x = (e.touches) ? e.touches[0].pageX : e.pageX;
-     var y = (e.touches) ? e.touches[0].pageY : e.pageY;
-
-     x = x - $("#canvas").offset().left
-     y = y - $("#canvas").offset().top
-
-     _.each(events, function(ev){
-      if (ev[0] <= x && x <= ev[0] + ev[2] && ev[1] <= y && y <= ev[1] + ev[3] && 'scroll_event' == ev[5]) {
-         ev[4](x, y)
-         drawPuzzle()
-       }
-     })
+  $('#setting_panel').hide()
+  if (supports_html5_storage()) {
+    if (localStorage.fullname) $('#fullname').val(localStorage.fullname)
+    if (localStorage.email) $('#email').val(localStorage.email)
   }
- }
- $(window).bind('click', mouseClick)
- window.onmousemove = mouseMove
- $('#saveButton')[0].onclick = function () {
-   if ($('#submit_details').is(':checked')) {
-     if ($('#fullname').val() == '') {
-       alert('Please fill the name field')
-       return false
-     }
-     if (supports_html5_storage()) {
-       localStorage.fullname = $('#fullname').val()
-       localStorage.email = $('#email').val()
-     }
-   }
-   $('#setting_panel').hide();
- }
- newGame()
+
+  window.ondblclick = mouseDoubleClick
+  function mouseMove(e) {
+    if (e.originalEvent) e = e.originalEvent
+    e = e || window.event
+    if (!e.which && e.button) {
+      if (e.button & 1) e.which = 1
+      else if (e.button & 4) e.which = 2
+      else if (e.button & 2) e.which = 3
+    }
+    if (e.buttons != undefined) button = e.buttons
+    else button = e.which
+    if (button) {
+      //  scroll if scrollbar is there
+
+      var x = e.touches ? e.touches[0].pageX : e.pageX
+      var y = e.touches ? e.touches[0].pageY : e.pageY
+
+      x = x - $('#canvas').offset().left
+      y = y - $('#canvas').offset().top
+
+      _.each(events, function(ev) {
+        if (
+          ev[0] <= x &&
+          x <= ev[0] + ev[2] &&
+          ev[1] <= y &&
+          y <= ev[1] + ev[3] &&
+          'scroll_event' == ev[5]
+        ) {
+          ev[4](x, y)
+          drawPuzzle()
+        }
+      })
+    }
+  }
+  $(window).bind('click', mouseClick)
+  window.onmousemove = mouseMove
+  $('#saveButton')[0].onclick = function() {
+    if ($('#submit_details').is(':checked')) {
+      if ($('#fullname').val() == '') {
+        alert('Please fill the name field')
+        return false
+      }
+      if (supports_html5_storage()) {
+        localStorage.fullname = $('#fullname').val()
+        localStorage.email = $('#email').val()
+      }
+    }
+    $('#setting_panel').hide()
+  }
+  newGame()
 })
 
 function isConnected(board, i, j) {
-  if(board[i] && board[i][j] && board[i][j] != -1) {
+  if (board[i] && board[i][j] && board[i][j] != -1) {
     board[i][j] = -1
     isConnected(board, i - 1, j)
     isConnected(board, i + 1, j)
